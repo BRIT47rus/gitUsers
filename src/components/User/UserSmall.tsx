b026b4699a8d194da77c5a88fef637b03a4e2170
Vladimir Noutbook <vovirus2@inbox.ru>
2025-08-04 14:32:38 +0300

    css grid compleated

--- src/components/User/UserSmall.tsx
+++ src/components/User/UserSmall.tsx
@@ -7,7 +7,19 @@ interface UserProps {
 export const UserSmall: FC<UserProps> = ({ gridArea }) => {
   return (
     <div className="user" style={{ gridArea: `u${gridArea}` }}>
-      User{gridArea}
+      <div className="user__avatar">
+        <img
+          src="https://sun9-1.userapi.com/s/v1/ig2/YSRewIzbN0Ngk73DrcVCmGsZHIMKUFW2h_wyTbjbUb2fd4ELP2_SDGtl5gr31BVgb_MemqzphpQe54zlqjIfzxeU.jpg?quality=95&as=32x33,48x49,72x74,108x111,160x164,240x246,360x369,480x492,540x553,640x655,720x737,1000x1024&from=bu&cs=1000x0"
+          alt="avatar"
+          className="user__avatar-img "
+        />
+      </div>
+      <div className="user__info">
+        <span className="user__info-name">Defunk </span>
+        <span className="user__info-desc">
+          15 репозиториев <p>Название организации</p>
+        </span>
+      </div>
     </div>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -3,49 +3,61 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.tsx',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.[contenthash].js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-                exclude: /node_modules/,
-            },
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
 
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(ts|tsx)$/,
-                loader: 'ts-loader',
-                exclude: /node_modules/,
-            },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
 
-            {
-                test: /\.svg$/,
-                type: 'asset/resource',
+      {
+        test: /\.svg$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192, // Если изображение меньше 8кб, оно будет встроено в бандл
+              name: 'images/[name].[ext]', // Путь и имя файла в итоговой сборке
             },
+          },
         ],
-    },
-    resolve: {
-        extensions: ['.js', '.ts', '.tsx'],
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: './src/index.html',
-        }),
-        new ESLintPlugin({
-            extensions: ['js', 'ts', 'tsx'],
-        }),
+      },
     ],
-    devServer: {
-        open: true,
-        historyApiFallback: true,
-    },
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new ESLintPlugin({
+      extensions: ['js', 'ts', 'tsx'],
+    }),
+  ],
+  devServer: {
+    open: true,
+    historyApiFallback: true,
+  },
 };
